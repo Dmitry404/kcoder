b24191ff38f47a7d54fa9cd20e50abc53277f56d
Dmitriy Butakov <dmitry404@gmail.com>
2018-04-09 11:31:01 +0300

    page 404

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,10 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { ExercisesListPageComponent } from './exercises-list-page/exercises-list-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/exercises', pathMatch: 'full' },
-  { path: 'exercises', component: ExercisesListPageComponent }
+  { path: 'exercises', component: ExercisesListPageComponent },
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
