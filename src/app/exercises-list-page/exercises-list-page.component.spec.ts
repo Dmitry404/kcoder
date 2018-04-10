c737455f455155eee363854f5fcb052bc5a6c543
Dmitriy Butakov <dmitry404@gmail.com>
2018-04-10 13:29:07 +0300

    Fixing failing tests

--- src/app/exercises-list-page/exercises-list-page.component.spec.ts
+++ src/app/exercises-list-page/exercises-list-page.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExercisesListPageComponent } from './exercises-list-page.component';
+import { ExercisesService } from './exercises.service';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 
 describe('ExercisesListPageComponent', () => {
   let component: ExercisesListPageComponent;
@@ -8,9 +11,14 @@ describe('ExercisesListPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ExercisesListPageComponent]
-    })
-      .compileComponents();
+      declarations: [ExercisesListPageComponent],
+    }).overrideComponent(ExercisesListPageComponent, {
+      set: {
+        providers: [
+          { provide: ExercisesService, useClass: ExercisesServiceMock },
+        ]
+      }
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -30,3 +38,18 @@ describe('ExercisesListPageComponent', () => {
     expect(exercises.length).not.toEqual(0);
   });
 });
+
+class ExercisesServiceMock extends ExercisesService {
+  constructor() {
+    super(null);
+  }
+
+  fetchExercises(): Observable<{ id: string, content: string }[]> {
+    return of([
+      {id: 'id1', content: 'id1_content'},
+      {id: 'id2', content: 'id2_content'},
+      {id: 'id3', content: 'id3_content'},
+      {id: 'id4', content: 'id4_content'},
+    ]);
+  }
+}
