0559912ed0b5d08c08eae354c9747003deacf791
Dmitriy Butakov <dmitry404@gmail.com>
2018-04-11 15:36:31 +0300

    Simple implementation of ExerciseTrainZoneComponent elements

--- src/app/exercise-train-zone/exercise-train-zone.component.ts
+++ src/app/exercise-train-zone/exercise-train-zone.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercise-train-zone.component.css']
 })
 export class ExerciseTrainZoneComponent implements OnInit {
+  code: string;
+  options = {
+    lineNumbers: true,
+    tabSize: 2,
+    mode: 'javascript',
+  };
+
+  runStatuses: RunStatus [] = [RunStatus.NOT_RUN];
 
   constructor() { }
 
@@ -13,6 +21,26 @@ export class ExerciseTrainZoneComponent implements OnInit {
   }
 
   onSubmitKataClick() {
-    console.log('submitted');
+    this.runStatuses.push(this.randomStatus());
   }
+
+  private randomStatus(): RunStatus {
+    return Math.floor(Math.random() * Object.keys(RunStatus).length / 2);
+  }
+
+  getClassName(runStatus: RunStatus): string {
+    switch(runStatus) {
+      case RunStatus.RED: return 'red';
+      case RunStatus.YELLOW: return 'yellow';
+      case RunStatus.GREEN: return 'green';
+      case RunStatus.NOT_RUN: return 'not-run';
+    }
+  }
+}
+
+enum RunStatus {
+  RED,
+  YELLOW,
+  GREEN,
+  NOT_RUN
 }
